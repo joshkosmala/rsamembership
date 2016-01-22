822aabadde79666c1debf606fd70b7d4f3cec3af
Josh Kosmala <josh@novaweb.co.nz>
2016-01-22 03:33:38 +0000

    Match up the old membership types

---
+++ mysite/tasks/ReinstateMembershipTypes.php
@@ -0,0 +1,51 @@
+<?php
+class ReinstateMembershipTypes extends BuildTask {
+
+    protected $title = 'Reinstate Membership Types';
+
+    protected $description = 'Populates the database with the default membership types';
+
+    //protected $enabled = true;
+
+    function run($request) {
+      // list the different membership types
+        /*$types = array(
+           'Associate',
+           'Returned',
+           'Returned Senior',
+           'Returned Life',
+           'Service',
+           'Service Senior',
+           'Service Life',
+           'Honorary',
+           'Honorary Life',
+           'Associate Senior',
+           'Service 90+',
+           'Returned 90+',
+           'Associate 90+'
+        );*/
+
+	$members = Member::get();
+
+        foreach($members as $member){
+	$type = $member->OldMembershipType;
+	if($type){
+	//	Debug::show($type);die();
+	$group = MembershipType::get()->filter(array('Title'=>$type))->first();
+	if($group){
+	$member->add($group);
+	//Debug::show($type);
+//Debug::show($group);
+//die();		
+}
+	}
+	
+          // $t = MembershipType::create();
+           //$t->Title = $type;
+           //$t->write();
+        }
+
+        echo "Membership types reinstated.";
+
+    }
+}
